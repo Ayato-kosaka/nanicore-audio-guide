fc82758dad264690be92f1002042053a8a77e760
Ayato-kosaka <kosaka.ayato@gmail.com>
2025-04-24 18:03:54 +0900

    fix #78 EAS Build (iOS internal distribution) がCannot find module 'expo/config'で失敗

--- app-expo/metro.config.js
+++ app-expo/metro.config.js
@@ -1,4 +1,4 @@
-const { getDefaultConfig } = require("@expo/metro-config");
+const { getDefaultConfig } = require("expo/metro-config");
 const path = require("path");
 
 // Find the project and workspace directories
@@ -8,17 +8,16 @@ const monorepoRoot = path.resolve(projectRoot, "..");
 const config = getDefaultConfig(projectRoot);
 
 config.resolver.extraNodeModules = {
-    "@shared": path.resolve(monorepoRoot, "shared"),
-    "@expo": path.resolve(monorepoRoot, "expo"),
-    "expo": path.resolve(monorepoRoot, "expo", "node_modules", "expo"),
+    '@shared': path.resolve(monorepoRoot, 'shared'),
+    '@expo': path.resolve(monorepoRoot, 'expo'),
 };
 
 // 1. Watch all files within the monorepo
 config.watchFolders = [monorepoRoot];
 // 2. Let Metro know where to resolve packages and in what order
 config.resolver.nodeModulesPaths = [
-    path.resolve(projectRoot, 'node_modules'),
-    path.resolve(monorepoRoot, 'node_modules'),
+    path.resolve(projectRoot, "node_modules"),
+    path.resolve(monorepoRoot, "node_modules"),
 ];
 
 module.exports = config;
